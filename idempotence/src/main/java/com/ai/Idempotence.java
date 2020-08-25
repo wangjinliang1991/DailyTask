1d323dcfe6b895f9dbeb6357508b81b3aca54800
wangjinliang1991 <wjl31802@126.com>
2020-08-25 17:28:56 +0800

    幂等项目的code review

--- idempotence/src/main/java/com/ai/Idempotence.java
+++ idempotence/src/main/java/com/ai/Idempotence.java
@@ -15,8 +15,13 @@ import java.util.UUID;
  * @Version: 1.0
  **/
 public class Idempotence {
+    //comment-1: 如果要替换存储方式，是否很麻烦
     private JedisCluster jedisCluster;
+    
+    //comment-2: 如果幂等框架要跟业务系统复用jedisCluster连接呢
+    //comment-3: 是否应该注释说明redisClusterAddress的格式，config是否可传递
     public Idempotence(String redisClusterAddress, GenericObjectPoolConfig config){
+        //comment-4: 这段逻辑放到构造函数，不容易写单元测试
         String[] addressArray = redisClusterAddress.split(";");
         Set<HostAndPort> redisNodes = new HashSet<>();
         for (String address:addressArray){
@@ -26,10 +31,13 @@ public class Idempotence {
         this.jedisCluster = new JedisCluster(redisNodes, config);
     }
     
+    //comment-5: generateId()是否更好点
+    //comment-6: 根据接口隔离原则，这个函数和其他函数的使用场景完全不同
     public String genId(){
         return UUID.randomUUID().toString();
     }
     
+    //comment-7: 返回值的意义是否应该注释说明下
     public boolean saveIfAbsent(String idempotenceId){
         Long success = jedisCluster.setnx(idempotenceId,"1");
         return success==1;
